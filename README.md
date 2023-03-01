ee1300411a90990fbded95e478c0431287961093
allons-y-rod <rodrigomarinho.eq@gmail.com>
2023-03-01 19:28:09 -0300

    chatroom_alert

--- README.md
+++ README.md
@@ -1,4 +1,7 @@
 # Airflow-utils
 ---
 A collection of Airflow scripts to help build data processing pipelines on GCP.
-
+---
+# gcp-chatroom-alert
+---
+Função a ser adicionada na DAG para em caso de falha um alerta seja emitido em um Chat Room da GCP
---
+++ gcp-chatroom-alert/chatroom_alert_func.py
@@ -0,0 +1,27 @@
+import requests
+from datetime import timedelta, datetime
+
+def alert_failed_task(context):
+    task_id = str(context.get('task_instance').task_id)
+    dag_id = str(context.get('task_instance').dag_id)
+    execution_date = (context.get('execution_date')-timedelta(hours=3)).strftime("%Y-%m-%d %H:%M:%S") #fuso horário
+    log_url = str(context.get('task_instance').log_url)
+    error_message = str(context.get('exception'))
+    if len(error_message) > 500:
+        error_message = error_message[:200] + \
+            '...Error message size is too long, see log below for more details'
+    else:
+        error_message
+    url = 'INSIRA O WEBHOOK AQUI'
+    message = f'''{{
+        "text":
+        "Task Failed:
+            Dag: {dag_id}
+            Task: {task_id}
+            Execution Date: {execution_date}
+            Error: {error_message}
+            Log Url: <{log_url}|Clique aqui para ver os logs>"
+    }}'''
+    headers = {'content-type': 'application/json', 'Accept-Charset': 'UTF-8'}
+    r = requests.post(url, data=message, headers=headers)
+    return r
